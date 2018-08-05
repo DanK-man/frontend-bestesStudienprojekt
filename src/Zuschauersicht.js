5972d655b1f52528f9bd0e6eeb43d6b79b115a89
Danny <s-dmanh2@haw-landshut.de>
2018-08-05 16:39:36 +0200

    Kommentar

--- src/Zuschauersicht.js
+++ src/Zuschauersicht.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ActionBar from './ActionBar';
 
 // Dummy-Komponente für die eigentliche Zuschauersicht
+// Chris, das hier mit deinem ersetzen
 function Zuschauersicht(props) {
   return (
     <div>
